2695126e03374826298da5b8690aea49cb313c7d
Gilles Stenger <gilles@gillesstenger.fr>
2021-11-05 08:49:28 +0100

    Load Data Model

--- Main/devdiagram.py
+++ Main/devdiagram.py
@@ -1,45 +1,66 @@
-class ObjectDescription(object):
-	className=""
-	classAttributes=[]
-	classInheritancyParent=None
-	classInheritancyChilds=[]
-	classOperations=[]
-	
-class Operation(object):
-	operationName=""
-	operationParameters=[]
-	operationReturns=[]
-	
-class Signature(object):
-	signatureName=""
-	signatureType=""
-	
 import os.path
-from types import SimpleNamespace
-import zipfile
 import json
+import zipfile
+from types import SimpleNamespace
 
-datamodel=[]
 
-def _GenerateData(topic=None,parent=None):
-    newClass=ObjectDescription()
-    newClass.className=topic.title
-	if hasattr(topic,'labels'):
-	   pass    
-    newClass.classInheritancyParent=parent
-    
+class ObjectDescription(object):
+    className = ""
+    classAttributes = []
+    classInheritancyParent = None
+    classInheritancyChilds = []
+    classOperations = []
+
+class Operation(object):
+    operationName = ""
+    operationParameters = []
+    operationReturns = []
+
+class Signature(object):
+    signatureName = ""
+    signatureType = ""
+    cardinality = ""
+
+datamodel = []
+
+def _GenerateData(topic=None, parent=None):
+    newClass = ObjectDescription()
+    newClass.className = topic.title
+    if hasattr(topic, 'labels'):
+        s=Signature()
+        for i in topic.labels:
+            if ":" in i:
+                l=i.split(":")
+                s.signatureName=l[0]
+                s.signatureType=l[2]
+                s.cardinality=l[1]
+            else:
+                s.signatureName=i
+                s.signatureType=None
+                s.cardinality=None
+            newClass.classAttributes.append(s)
+    newClass.classInheritancyParent = parent
+    if hasattr(topic,'children'):
+        if hasattr(topic.children,'attached'):
+            for i in topic.children.attached:
+                newClass.classInheritancyChilds.append(_GenerateData(topic=i,parent=newClass))
+        if hasattr(topic.children,'detached'):
+            for i in topic.children.detached:
+                newClass.classInheritancyChilds.append(_GenerateData(topic=i,parent=newClass))
+    return newClass
 
 def _loadXmind(name=None):
-	with zipfile.ZipFile(name) as f:
-		if "content.json" in f.namelist():
-			data=f.open("content.json").read().decode('utf-8')
-	sh=json.loads(data,object_hook=lambda d: SimpleNamespace(**d))
-	for i in sh:
-		if "Inheritancy" in i.title:
-			_GenerateData(topic=i.rootTopic)
+    with zipfile.ZipFile(name) as f:
+        if "content.json" in f.namelist():
+            data = f.open("content.json").read().decode('utf-8')
+    sh = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+    for i in sh:
+        if "Inheritancy" in i.title:
+            datamodel.append(_GenerateData(topic=i.rootTopic))
+    return datamodel
 
 def loadDevDiagram(name=None):
-	ret=None
-	if os.path.splitext(name)[1] == ".xmind":
-		ret=_loadXmind(name=name)
-	return ret
+    ret = None
+    if os.path.splitext(name)[1] == ".xmind":
+        ret = _loadXmind(name=name)
+    return ret
--- Main/devprojectmgr.py
+++ Main/devprojectmgr.py
@@ -1,45 +1,48 @@
 import sys
 sys.path.append("..")
 sys.path.append(".")
-from kivy.app import App
-from kivy.logger import Logger
-from kivy.properties import StringProperty
-
-#kivy.require('1.0.6')
-from kivy.uix.scatter import Scatter
-
-from devdiagram import loadDevDiagram
-
+from Lib import getUserPath
 from Lib.filedialog import FileDialog
-from Lib import getUserPath  
+from devdiagram import loadDevDiagram
+from kivy.uix.scatter import Scatter
+from kivy.properties import StringProperty
+from kivy.logger import Logger
+from kivy.app import App
 
-class SchemaObject(Scatter):  
+# kivy.require('1.0.6')
+
+class SchemaObject(Scatter):
     source = StringProperty(None)
 
 class ProjectApp(App):
-    workspaceRoot=StringProperty(getUserPath())
     
-    def on_workspaceRoot(self,instance,value):
-    	self.root.ids.title.text=value
-    	
+    workspaceRoot = StringProperty(getUserPath())
+
+    def on_workspaceRoot(self, instance, value):
+        self.root.ids.title.text = value
+
     def _loadfile(self):
-    	self.devdiagram=loadDevDiagram(name=self.workspaceRoot)
-    
+        self.devdiagram = loadDevDiagram(name=self.workspaceRoot)
+        pass
+
     def _create_popup_workspace_open(self):
-        if not hasattr(self,"openFile"):
-            self.openFile=FileDialog(sourceObject=self,sourceProperty="workspaceRoot",title="Open Schema",onsubmit=self._loadfile)
+        if not hasattr(self, "openFile"):
+            self.openFile = FileDialog(
+                sourceObject=self, sourceProperty="workspaceRoot", title="Open Schema", onsubmit=self._loadfile)
         else:
             if self.openFile is None:
-                self.openFile=FileDialog(sourceObject=self,sourceProperty="workspaceRoot",title="Open Schema",onsubmit=self._loadfile)
+                self.openFile = FileDialog(
+                    sourceObject=self, sourceProperty="workspaceRoot", title="Open Schema", onsubmit=self._loadfile)
         self.openFile.showDialog()
-        
+
     def build(self):
-    	value=getUserPath()
-    	self.workspaceRoot=value
-    	root = self.root
-    	
+        value = getUserPath()
+        self.workspaceRoot = value
+        root = self.root
+
     def on_pause(self):
         return True
+
 
 if __name__ == '__main__':
     ProjectApp().run()
