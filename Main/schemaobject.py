9ed72fcc531451065912ebb55519f069a69705b5
gilles@gillesstenger.fr <gilles@gillesstenger.fr>
2021-11-15 07:21:17 +0100

    backup

--- Main/schemaobject.py
+++ Main/schemaobject.py
@@ -1,16 +1,29 @@
 from kivy.properties import StringProperty
-from kivy.uix.scatter import Scatter
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.graphics import Canvas
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
 from kivy.app import App
 from kivy.core.window import Window
 
-class SchemaObject(Scatter):
-    source = StringProperty(None)
-
+class SchemaObject(Widget):
+    
+    def on_touch_move(self,touch):
+    	self.pos=touch.pos
+    	
+    def redraw(self,*args):
+    	with self.canvas:
+    		c=Color(1.,1.,1.)
+    		Rectangle(pos=self.pos,size=self.size)
+	
 def cleanUi():
 	pass
 	
 def createUiGraph(g=None):
 	for i in g:
 		newWidget=SchemaObject()
-		newWidget.ids.title.text=i.className
+#		newWidget.add_widget(Label(text=i.className))
+		newWidget.size_hint=(0.1,0.1)
+		newWidget.bind(pos=newWidget.redraw,size=newWidget.redraw)
 		App.get_running_app().root.add_widget(newWidget)
