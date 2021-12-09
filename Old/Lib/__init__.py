692206349584714b48de07962868d242710d7080
stengergilles <gilles@gillesstenger.fr>
2021-12-09 16:59:44 +0100

    Moved to Old

---
+++ Old/Lib/__init__.py
@@ -0,0 +1,16 @@
+from kivy.utils import platform
+import os
+
+def getUserPath():
+	if platform == "android":
+		try:
+			from jnius import autoclass
+			env = autoclass('android.os.Environment').getExternalStorageDirectory().getPath()
+			return(env)
+		except Exception as a:
+			return(None)
+	if platform == "win":
+        return(os.getenv("USERPROFILE"))
+    if platform == "linux":
+    	return(os.getenv("HOME"))
+	return(None)
