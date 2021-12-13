edf643bd06a1958dcb3867aac77f9f0f9b53f470
stengergilles <gilles@gillesstenger.fr>
2021-12-13 08:34:20 +0100

    WindowScroll

--- Main.py
+++ Main.py
@@ -21,6 +21,7 @@ class SchemaApp(App):
 	principal=None
 	ouvrir=None
 	close=None
+	scrollpos=(0,0)
 
 	workspaceRoot = StringProperty(getUserPath())
 
@@ -34,15 +35,7 @@ class SchemaApp(App):
 					sourceObject=self, sourceProperty="workspaceRoot", title="Open Schema", onsubmit=self._loadCanvas)
 		self.ouvrirFile.showDialog()
 
-	def on_window_resize(self,window,width,height):
-		for i in self.principal.children:
-			if type(i) is CircularButton:
-				i.pos=((1 - i.factor * i.size[0]/window.width)*window.width,20)
-
 	def _loadCanvas(self):
-		print("load canvas")
-		print(self.principal)
-		print(self.racine)
 		for i in self.racine.children:
 			if type(i) is SchemaObject:
 				self.racine.remove_widget(i)
@@ -50,28 +43,51 @@ class SchemaApp(App):
 		newWidget.pos=(randint(50,100),randint(50,100))
 		self.racine.add_widget(newWidget)
 
+	def on_window_resize(self,window,width,height):
+		for i in self.racine.children:
+			if type(i) is CircularButton:
+				i.pos=((1 - i.factor * i.size[0]/window.width)*window.width,20)
+
+	def on_touch_down(self,floatlayout,touch):
+		for i in self.racine.children:
+			if i.collide_point(*touch.pos):
+				return i.on_touch_down(touch)
+		self.moving=True
+		self.prevpos=touch.pos
+
+	def on_touch_up(self,floatlayout,touch):
+		for i in self.racine.children:
+			if i.collide_point(*touch.pos):
+				return i.on_touch_up(touch)	
+		self.moving=False
+		self.prevpos=None
+
+	def on_touch_move(self,floatlayout,touch):
+		for i in self.racine.children:
+			if i.collide_point(*touch.pos):
+				return i.on_touch_move(touch)		
+		if self.moving:
+			delta=((touch.pos[0]-self.prevpos[0]),(touch.pos[1]-self.prevpos[1]))
+			self.prevpos=(touch.pos[0],touch.pos[1])
+			for i in self.racine.children:
+				if type(i) is SchemaObject:
+					i.pos=(i.pos[0]+delta[0],i.pos[1]+delta[1])
+
 	def build(self):
-		self.racine=FloatLayout(size_hint=(0.8,0.8))
-		self.principal=FloatLayout(size=(Window.width,Window.height))
-		print("build")
-		print(self.racine)
-		print(self.principal)
+		self.racine=FloatLayout(size=(Window.width,Window.height))
+		self.racine.bind(on_touch_down=self.on_touch_down,on_touch_up=self.on_touch_up,on_touch_move=self.on_touch_move)
 		CustomGraphics.SetBG(self.racine,bg_color=[0.5,0.5,0.5,0.5])
-		CustomGraphics.SetBG(self.principal,bg_color=[0.5,0.5,0.5])
-		scrlv=ScrollView()
-		scrlv.add_widget(self.racine)
-		self.ouvrir=CircularButton(img='fileopen.png',pos=((1 - dp(64)/Window.width)*    Window.width,dp(20)),size=(dp(64),dp(64)),size_hint=(None,None))
+		self.ouvrir=CircularButton(img='fileopen.png',pos=((1 - dp(64)/Window.width)*Window.width,dp(20)),size=(dp(64),dp(64)),size_hint=(None,None))
 		self.ouvrir.factor=1
 		self.ouvrir.bind(pos=self.ouvrir.redraw,size=self.ouvrir.redraw,on_press=self._create_popup_workspace_ouvrir)
-		self.principal.add_widget(self.ouvrir)
+		self.racine.add_widget(self.ouvrir)
 		self.close=CircularButton(img='fileclose.png',pos=(int((1 - 2 * dp(64)/Window.width)*Window.width),int(dp(20))),size=(int(dp(64)),int(dp(64))),size_hint=(None,None))
 		self.close.factor=2
 		self.close.bind(pos=self.close.redraw,size=self.close.redraw)
-		self.principal.add_widget(self.close)
-		self.principal.add_widget(scrlv)
+		self.racine.add_widget(self.close)
 		self._loadCanvas()
 		Window.bind(on_resize=self.on_window_resize)
-		return self.principal
+		return self.racine
 
 if __name__ == '__main__':
 	SchemaApp().run()
