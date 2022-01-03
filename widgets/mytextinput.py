9aad9319285db4c983f551e2c52fc41845ccfb02
stengergilles <gilles@gillesstenger.fr>
2022-01-03 09:42:41 +0100

    state

--- widgets/mytextinput.py
+++ widgets/mytextinput.py
@@ -5,5 +5,8 @@ class MyTextInput(TextInput):
 	def __getstate__(self):
 		return {"text": self.text}
 
+	def __setstate__(self,state):
+		self.text=state["text"]
+
 	def __init__(self,**kwargs):
 		super(MyTextInput,self).__init__(**kwargs)
