ecd5981aa8b99901ccf862c0765c271ff3a1df5e
stengergilles <gilles@gillesstenger.fr>
2022-01-14 14:26:16 +0100

    readded scrollpos

--- widgets/myfloatlayout.py
+++ widgets/myfloatlayout.py
@@ -22,7 +22,7 @@ class MyFloatLayout(FloatLayout):
               if self._child_exist(child=self.menu):
                  self.remove_widget(self.menu)
               return(i.on_touch_down(touch))
-        if not self._child_exist(child=self.menu):
+        if not self._child_exist(child=self.menu) and touch.is_double_tap:
            self.add_widget(self.menu)
            self.menu.center_x=touch.pos[0]
            self.menu.center_y=touch.pos[1]
@@ -30,13 +30,19 @@ class MyFloatLayout(FloatLayout):
            for i in self.menu.children:
                if i.collide_point(*touch.pos):
                   return i.on_touch_down(touch)
+           if not touch.is_double_tap:
+              self.moving=True
            self.remove_widget(self.menu)
 
     def on_touch_up(self,touch):
-        pass
+        if hasattr(self,"moving"):
+           self.moving=False
 
     def on_touch_move(self,touch):
-        pass
+        if hasattr(self,"moving"):
+           if self.moving:
+              for i in self.children:
+                  i.pos=(i.pos[0]+touch.dx,i.pos[1]+touch.dy)
 
     def __init__(self,menu=None, **kwargs):
         super(MyFloatLayout, self).__init__(**kwargs)
