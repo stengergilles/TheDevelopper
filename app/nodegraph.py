757b78da0ecc8ab7ee5dd1a739f475a761322527
Gilles <gilles@gillesstenger.fr>
2022-02-07 10:30:00 +0000

    Revert "pos hint does not work"
    This reverts commit dfdf3925f805a7ec069cc11469a62361f6ebbb8a

--- app/nodegraph.py
+++ app/nodegraph.py
@@ -13,10 +13,9 @@ class NodeGraph(SchemaObject):
 	def redraw(self,*args):
 		super(NodeGraph,self).redraw(args)
 		c=(self.c.center_x,self.c.center_y)
-		self.l.pos=(self.size[0] - self.l.texture_size[0], self.size[1] - self.l.texture_size[1])
 		with self.canvas.before:
 			Color(0,0,0)
-			Line(points=[c, (c[0],self.size[1] - self.l.texture_size[1]),(self.width,self.size[1] - self.l.texture_size[1])])
+			Line(points=[c, (c[0],self.l.pos[1]),(self.width,self.l.pos[1])])
 		
 	def __init__(self,data=None,**kwargs):
 		super(NodeGraph,self).__init__(data=data,**kwargs)
@@ -31,7 +30,7 @@ class NodeGraph(SchemaObject):
 		else:
 			t='Untitled Node'
 		self.c=MDIconButton(icon=i,pos=(0,0),size_hint=(None,None),size=(dp(16),dp(16)))
-		self.l=MDLabel(text=t,size_hint=(1,None),halign='right',font_style='Caption')
+		self.l=MDLabel(text=t,size_hint=(1,None),pos_hint={'top':1,'right':1},halign='right',font_style='Caption')
 		self.add_widget(self.l)
 		self.add_widget(self.c)
 		self.bind(pos=self.redraw,size=self.redraw)
