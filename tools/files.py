3d328aa28000ca831b5fdbf97d6aa2b1c907af07
stengergilles <gilles@gillesstenger.fr>
2022-02-23 11:10:12 +0100

    Load Ok but towidget ki

--- tools/files.py
+++ tools/files.py
@@ -23,10 +23,9 @@ def haveref(id=None):
 	print(id)
 	for i in app.settings.schema:
 		for k,v in i.items():
-			print(str(k))
-			print(str(v))
-			if str(v) == id:
-				return(i,k)
+			if hasattr(v,'toresolve'):
+				if v.toresolve == id.hex:
+					return(i,k)
 	return None
 	
 def loadschema(path=None):
@@ -37,13 +36,11 @@ def loadschema(path=None):
 			for i in app.settings.toresolv:
 				if hasattr(i,'toresolve'):
 					r.append(resolve(i.toresolve))
-					app.settings.toresolv.remove(i)
 			for i in r:
-				z=haveref(i)
+				z=haveref(i['uuid'])
 				print('after')
 				if z:
 					z[0][z[1]]=i
-					print(z)
 			return(True)
 	except Exception as e:
 		print(e)
